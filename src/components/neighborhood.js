2d0b076c6262771398fa0781e65971df4705bd64
brandendupont <branden.dupont@gmail.com>
2020-06-08 11:38:07 -0500

    update website

--- src/components/neighborhood.js
+++ src/components/neighborhood.js
@@ -26,7 +26,7 @@ const Container = styled.div`
 
 
 // defined two iframes
-const srcs = {'chart':'https://datawrapper.dwcdn.net/hPCKw/7/', 'map':'https://datawrapper.dwcdn.net/9zRw1/2/'}
+const srcs = {'chart':'https://datawrapper.dwcdn.net/hPCKw/8/', 'map':'https://datawrapper.dwcdn.net/9zRw1/3/'}
 
 const tabColor = ['#EDEDED', 'white']
 
